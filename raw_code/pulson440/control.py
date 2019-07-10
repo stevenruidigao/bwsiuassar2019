50d332c44ada8779c976c676c6fdef4de5601b07
Steven Gao <stevenruidigao@gmail.com>
2019-07-10 14:59:41 -0400

    Add collect_mode

--- raw_code/pulson440/control.py
+++ raw_code/pulson440/control.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 """Command and control script for PulsON 440 via Pi."""
 
-__author__ = 'Ramamurthy Bhagavatula, Michael Riedl'
-__version__ = '1.0'
-__maintainer__ = 'Ramamurthy Bhagavatula'
-__email__ = 'ramamurthy.bhagavatula@ll.mit.edu'
+__author__ = "Ramamurthy Bhagavatula, Michael Riedl"
+__version__ = "1.0"
+__maintainer__ = "Ramamurthy Bhagavatula"
+__email__ = "ramamurthy.bhagavatula@ll.mit.edu"
 
 # Update path
 from pathlib import Path
 import sys
-if Path('..//').resolve().as_posix() not in sys.path:
-    sys.path.insert(0, Path('..//').resolve().as_posix())
+if Path("..//").resolve().as_posix() not in sys.path:
+    sys.path.insert(0, Path("..//").resolve().as_posix())
 
 # Import required modules and methods
 import argparse
@@ -24,10 +24,10 @@ import yaml
 
 # Logger setup
 try:
-    logger_config_filename = (Path(__file__).parent / 'log_config.yml').resolve().as_posix()
-    with open(logger_config_filename, 'r') as f:
+    logger_config_filename = (Path(__file__).parent / "log_config.yml").resolve().as_posix()
+    with open(logger_config_filename, "r") as f:
         logger_config = yaml.load(f, Loader=yaml.FullLoader)
-    logger = setup_logger(name=logger_config['name'], config=logger_config['config'])
+    logger = setup_logger(name=logger_config["name"], config=logger_config["config"])
 except Exception as e:
     print(e)
     logger = setup_logger(name=DEFAULT_LOGGER_NAME, config=DEFAULT_LOGGER_CONFIG)
@@ -57,8 +57,8 @@ def parse_args(args):
     parser.add_argument("settings_file", type=str) # Path name of the radar settings file
     parser.add_argument("scan_data_filename", type=str) # Path name for the document that we want to save the data in
     parser.add_argument("scan_count", type=int) # Number of scans that we want to take
+    parser.add_argument("collect_mode", type=str, default="collect") # Collection mode; quicklook or collect
     parser.add_argument("--return_data", "--return", action="store_true") # Whether or not we want to return the data to save it in a variable; OPTIONAL
-    
     parsed_args = parser.parse_args(args) # Go through args, find the arguments, and save them into parsed_args
 ##    print(parsed_args)
     
@@ -92,11 +92,11 @@ def main(args):
     Raises:
         # TODO: Update w/ appropriate error cases.
     """
-    logger.info('Starting radar data collection process...')
+    logger.info("Starting radar data collection process...")
 
     # Parse input arguments
     parsed_args = parse_args(args)
-    logger.debug('Input arguments are --> {0}'.format(parsed_args))
+    logger.debug("Input arguments are --> {0}".format(parsed_args))
     
     # Initialize output
     data = None
@@ -111,16 +111,16 @@ def main(args):
         radar.connect()
         radar.read_settings_file(settings_file=parsed_args.settings_file)
         radar.set_radar_config()
-        if parsed_args.collect_mode == 'collect':
+        if parsed_args.collect_mode == "collect":
             data = radar.collect(scan_count=parsed_args.scan_count, scan_data_filename=parsed_args.scan_data_filename, return_data=parsed_args.return_data)#Insert arguments)
-        elif parsed_args.collect_mode == 'quick':
+        elif parsed_args.collect_mode == "quick":
             data = radar.quick_look(scan_data_filename=parsed_args.scan_data_filename, return_data=parsed_args.return_data)
         else:
-            raise RuntimeError('Unrecognized collection mode {0}'.format(parsed_args.collect_mode))
-        logger.info('Completed radar data collection process!')
+            raise RuntimeError("Unrecognized collection mode {0}".format(parsed_args.collect_mode))
+        logger.info("Completed radar data collection process!")
 
     except Exception:
-        logger.exception('Fatal error encountered!')
+        logger.exception("Fatal error encountered!")
         
     # Disconnect radar and close logger
     finally:
@@ -129,7 +129,7 @@ def main(args):
         
     return data
     
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Standard Python alias for command line execution."""
     main(sys.argv[1:])
     
