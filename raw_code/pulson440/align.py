946a782b9083eee8e6a42b78a8f33dc2c8f1fe2e
pulomabishnu <51140602+pulomabishnu@users.noreply.github.com>
2019-07-23 16:18:17 -0400

    Update align.py
    Separated/defined new functions for organizational purposes, added comments

--- raw_code/pulson440/align.py
+++ raw_code/pulson440/align.py
@@ -1,8 +1,10 @@
-#imports pandas
+#imports desired libs
 import pandas
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+
+#opens and loads pickle file
 with open('Mandrill_1way_Misaligned1_data.pkl', 'rb') as f:
     data=pickle.load(f)
 
@@ -23,8 +25,8 @@ def panda_stuff(x):
     print(stuff)
     return stuff
 
-
-def data_align(scan_data, platform_pos, range_bins, scan_timestamps, motion_timestamps, corner_reflector_pos):
+#finds the time stamp at which the drone takes off relative to the motion capture system's timer
+def MC_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos):
     one_way_range = np.sqrt(np.sum(np.square(platform_pos - corner_reflector_pos[0]), axis=1))
     first_value = one_way_range[0]
     new = np.where(one_way_range==first_value, 0, one_way_range)
@@ -32,21 +34,26 @@ def data_align(scan_data, platform_pos, range_bins, scan_timestamps, motion_time
     tkf_scan_num = newnew[0][0]
     tkf_motion_timestamp = data['motion_timestamps'][tkf_scan_num]
     print("Time of takeoff after the motion capture device was turned on (seconds): ", tkf_motion_timestamp)
-    def find_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos):
-        one_way_range = np.sqrt(np.sum(np.square(platform_pos - corner_reflector_pos[0]), axis=1))
-        first_value = one_way_range[0]
-        cr_first_rbin = np.argmin(np.abs(first_value-range_bins))
-        num_scans = len(scan_data)
-        for k in range(1, num_scans):
-            yeet = scan_data[k, cr_first_rbin]
-            yeet_two = scan_data[k-1, cr_first_rbin]
-            if np.abs(yeet - yeet_two) > 4.5:
-                tkf_scan_timestamp = data['scan_timestamps'][k]
-                print("Time of takeoff after the radar was turned on (seconds): ", tkf_scan_timestamp)
-                return k
+
+#function within a function; finds the time stamp at which the drone takes off relative to the radar's timer
+def RD_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos):
+    one_way_range = np.sqrt(np.sum(np.square(platform_pos - corner_reflector_pos[0]), axis=1))
+    first_value = one_way_range[0]
+    cr_first_rbin = np.argmin(np.abs(first_value-range_bins))
+    num_scans = len(scan_data)
+    for k in range(1, num_scans):
+        yeet = scan_data[k, cr_first_rbin]
+        yeet_two = scan_data[k-1, cr_first_rbin]
+        if np.abs(yeet - yeet_two) > 4.5:
+            tkf_scan_timestamp = data['scan_timestamps'][k]
+            print("Time of takeoff after the radar was turned on (seconds): ", tkf_scan_timestamp)
+            return k
 
 
-    find_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos)
+#data align function (incomplete, nothing is there we just called our functions here so the data align function doesnt throw an error)
+def data_align(scan_data, platform_pos, range_bins, scan_timestamps, motion_timestamps, corner_reflector_pos):
+    MC_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos)
+    RD_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos)
 
 data_align(data['scan_data'], data['platform_pos'], data['range_bins'], data['scan_timestamps'], data['motion_timestamps'], data['corner_reflector_pos'])
 
