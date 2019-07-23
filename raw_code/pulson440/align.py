ac5126bf4fe4fa29469dc63406317959e17d154b
Manu-MA <52300975+Manu-MA@users.noreply.github.com>
2019-07-23 11:05:27 -0400

    Update align.py

--- raw_code/pulson440/align.py
+++ raw_code/pulson440/align.py
@@ -14,16 +14,21 @@ def panda_stuff(x):
 
 
 def data_align(scan_data, platform_pos, range_bins, scan_timestamps, motion_timestamps, corner_reflector_pos):
-    # finds range to corner reflector
     one_way_range = np.sqrt(np.sum(np.square(platform_pos - corner_reflector_pos[0]), axis=1))
     first_value = one_way_range[0]
-    # changes all values equal to the initial value to 0 and returns the index of the first non-zero value(the first MCS scan after takeoff)
-    new = np.where(one_way_range == first_value, 0, one_way_range)
+    new = np.where(one_way_range==first_value, 0, one_way_range)
     newnew = np.nonzero(new)
     tkf_scan_num = newnew[0][0]
-    # finds the MCS timestamp of takeoff
     tkf_motion_timestamp = data['motion_timestamps'][tkf_scan_num]
-    print(tkf_motion_timestamp)
+    #print(tkf_motion_timestamp)
+    cr_first_rbin = np.argmin(np.abs(first_value-range_bins))
+    num_scans = len(scan_data)
+    for k in range(1, num_scans):
+        yeet = scan_data[k, cr_first_rbin]
+        yeet_two = scan_data[k-1, cr_first_rbin]
+        if np.abs(yeet - yeet_two) > 4.5:
+            return k
+    return yeet
 
 
 data_align(data['scan_data'], data['platform_pos'], data['range_bins'], data['scan_timestamps'], data['motion_timestamps'], data['corner_reflector_pos'])
