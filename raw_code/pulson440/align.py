7925c3310c10f7f7a3940d8e7e7210b595d2e0e4
Manu-MA <52300975+Manu-MA@users.noreply.github.com>
2019-07-24 10:52:05 -0400

    Update align.py
    Began aligning actual things

--- raw_code/pulson440/align.py
+++ raw_code/pulson440/align.py
@@ -25,15 +25,13 @@ def panda_stuff(x):
     print(stuff)
     return stuff
 
-#finds the time stamp at which the drone takes off relative to the motion capture system's timer
 def MC_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos):
     one_way_range = np.sqrt(np.sum(np.square(platform_pos - corner_reflector_pos[0]), axis=1))
     first_value = one_way_range[0]
     new = np.where(one_way_range==first_value, 0, one_way_range)
     newnew = np.nonzero(new)
     tkf_scan_num = newnew[0][0]
-    tkf_motion_timestamp = data['motion_timestamps'][tkf_scan_num]
-    print("Time of takeoff after the motion capture device was turned on (seconds): ", tkf_motion_timestamp)
+    return tkf_scan_num
 
 #function within a function; finds the time stamp at which the drone takes off relative to the radar's timer
 def RD_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos):
@@ -42,18 +40,23 @@ def RD_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos):
     cr_first_rbin = np.argmin(np.abs(first_value-range_bins))
     num_scans = len(scan_data)
     for k in range(1, num_scans):
-        yeet = scan_data[k, cr_first_rbin]
+        yeet_one = scan_data[k, cr_first_rbin]
         yeet_two = scan_data[k-1, cr_first_rbin]
-        if np.abs(yeet - yeet_two) > 4.5:
-            tkf_scan_timestamp = data['scan_timestamps'][k]
-            print("Time of takeoff after the radar was turned on (seconds): ", tkf_scan_timestamp)
+        if np.abs(yeet_one - yeet_two) > 4.5:
             return k
-
 
 #data align function (incomplete, nothing is there we just called our functions here so the data align function doesnt throw an error)
 def data_align(scan_data, platform_pos, range_bins, scan_timestamps, motion_timestamps, corner_reflector_pos):
-    MC_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos)
-    RD_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos)
+    yeet = MC_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos)
+    yote = RD_tkf_timestamp(scan_data, platform_pos, range_bins, corner_reflector_pos)
+    print(data['scan_timestamps'])
+    real_scan_times = data['scan_timestamps'] - data['scan_timestamps'][0]
+    print(real_scan_times)
+    tkf_motion_timestamp = data['motion_timestamps'][yeet]
+    tkf_scan_timestamp = real_scan_times[yote]
+    aligned_motion_times = data['motion_timestamps'] + tkf_scan_timestamp - tkf_motion_timestamp
+    print(tkf_scan_timestamp, tkf_motion_timestamp)
+    print(aligned_motion_times)
 
 data_align(data['scan_data'], data['platform_pos'], data['range_bins'], data['scan_timestamps'], data['motion_timestamps'], data['corner_reflector_pos'])
 
