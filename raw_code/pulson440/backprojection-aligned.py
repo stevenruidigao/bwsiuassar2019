8aea9428dd8764c56933cabc94ad8e7e9e92a1ce
stevenruidigao <stevenruidigao@gmail.com>
2019-07-30 15:14:02 -0400

    Add croppy stuff

--- raw_code/pulson440/backprojection-aligned.py
+++ raw_code/pulson440/backprojection-aligned.py
@@ -18,17 +18,17 @@ if Path("..//").resolve().as_posix() not in sys.path:
     sys.path.insert(0, Path("..//").resolve().as_posix())
 from common.helper_functions import replace_nan
 
-def backprojection(data, start=-3, stop=3, resolution=0.05, twodimbins=False, mode="2dfast"):
+def backprojection(data, start=-3, stop=3, resolution=0.05, mode="2dfast"):
     if mode == "2dfast":
-        return backprojection2dfast(data, start, stop, resolution, twodimbins)
+        return backprojection2dfast(data, start, stop, resolution)
     elif mode =="3dfast":
-        return backprojection3dfast(data, start, stop, resolution, twodimbins)
+        return backprojection3dfast(data, start, stop, resolution)
     elif mode =="2dslow":
-        return backprojection2dslow(data, start, stop, resolution, twodimbins)
+        return backprojection2dslow(data, start, stop, resolution)
     else:
         raise RuntimeError("Invalid mode!")
 
-def backprojection2dfast(data, start=-3, stop=3, resolution=0.05, twodimbins=False):
+def backprojection2dfast(data, start=-3, stop=3, resolution=0.05):
     """Runs backprjection on SAR data
     
     Args:
@@ -50,7 +50,7 @@ def backprojection2dfast(data, start=-3, stop=3, resolution=0.05, twodimbins=Fal
     # Load the data
     scan_data = data["scan_data"]
     platform_pos = data["platform_pos"]
-    range_bins = data["range_bins"][0] if twodimbins else data["range_bins"]
+    range_bins = data["range_bins"]
     # The number of pixels per dimension
     alen = int((stop - start) / resolution) + 1
     xlen = alen
@@ -67,10 +67,10 @@ def backprojection2dfast(data, start=-3, stop=3, resolution=0.05, twodimbins=Fal
         return_data += np.flipud(interp) # Flip the returned intensities and add the intensities to the return_data array
     return np.abs(return_data)
 
-def backprojection3dfast(data, start=-3, stop=3, resolution=0.05, twodimbins=False):
+def backprojection3dfast(data, start=-3, stop=3, resolution=0.05):
     scan_data = data["scan_data"]
     platform_pos = data["platform_pos"]
-    range_bins = data["range_bins"][0] if twodimbins else data["range_bins"]
+    range_bins = data["range_bins"]
     alen = int((stop - start) / resolution) + 1
     xlen = alen
     ylen = alen
@@ -90,10 +90,10 @@ def backprojection3dfast(data, start=-3, stop=3, resolution=0.05, twodimbins=Fal
     plt.show()
     return np.abs(return_data)
 
-def backprojection2dslow(data, start=-3, stop=3, resolution=0.05, twodimbins=False):
+def backprojection2dslow(data, start=-3, stop=3, resolution=0.05):
     scan_data = data["scan_data"]
     platform_pos = data["platform_pos"]
-    range_bins = data["range_bins"][0] if twodimbins else data["range_bins"]
+    range_bins = data["range_bins"]
 ##    print(range_bins)
     alen = int((stop - start) / resolution) + 1
     xlen = alen
@@ -140,7 +140,7 @@ def RD_tkf_indexn(scan_data, platform_pos, range_bins, corner_reflector_pos):
     first_value = one_way_range[0]
     cr_first_rbin = np.argmin(np.abs(first_value - range_bins))
     num_scans = len(scan_data)
-    return (np.abs(scan_data[:, cr_first_rbin] - scan_data[0, cr_first_rbin]) > 5).nonzero()[0][0]
+    return (np.abs(scan_data[:, cr_first_rbin] - scan_data[0, cr_first_rbin]) > 0.5).nonzero()[0][0] # 11.208849086510625 
 
 def motion_align(data):
     scan_data = data['scan_data']
@@ -172,7 +172,9 @@ def motion_align(data):
     newdata['motion_timestamps'] = newdata['scan_timestamps']
     return newdata
 
-def range_norm(scan_data, range_bins):
+def range_norm(data):
+    scan_data = data['scan_data']
+    range_bins = data['range_bins']
     norm_scan_data = np.copy(scan_data)
     norm_scan_data *= ((range_bins / range_bins[0]) ** 4)
     return norm_scan_data
@@ -183,28 +185,101 @@ def replace_nans(data):
     newdata['range_bins'] = replacement_data[0]
     newdata['scan_data'] = replacement_data[1]
     return newdata
+
+def crop(data, start_range, end_range):
+##    data = olddata.copy()
+    scan_data = data['scan_data']
+    platform_pos = data['platform_pos']
+    range_bins = data['range_bins']
+    corner_reflector_pos = data['corner_reflector_pos']
+    motion_timestamps = data['motion_timestamps']
+    scan_timestamps = data['scan_timestamps']
+
+    mid = int(len(scan_timestamps) / 2)
+
+    rd_takeoff = mid - 30
+        # RD_tkf_indexn(scan_data, platform_pos, range_bins, corner_reflector_pos)
+
+    rd_landing = mid + 30
+        # RD_lnd_index(scan_data, platform_pos, range_bins, corner_reflector_pos)
+
+    print(type(start_range))
+    print(type(range_bins))
+##    print(range_bins - start_range)
+##    print(range_bins)
     
+    start_index = np.abs(range_bins - start_range).argmin()
+    end_index = np.abs(range_bins - end_range).argmin()
+
+    scan_data = scan_data[rd_takeoff:rd_landing, start_index:end_index]
+    scan_timestamps = scan_timestamps[rd_takeoff:rd_landing]
+
+    platform_pos = platform_pos[rd_takeoff:rd_landing]
+    motion_timestamps = motion_timestamps[rd_takeoff:rd_landing]
+
+    range_bins = range_bins[start_index:end_index]
+
+    data['scan_timestamps'] = scan_timestamps
+    data['scan_data'] = scan_data
+
+    data['platform_pos'] = platform_pos
+    data['motion_timestamps'] = motion_timestamps
+
+    data['range_bins'] = range_bins
+
+    return data
 
 parser = argparse.ArgumentParser(description=" - Runs backprojection on SAR data.")
-parser.add_argument("--filename", "-f", type=str, required=True, help=" - The SAR data filename")
-parser.add_argument("--start", type=float, default=-3, help=" - The start of the coordinate plane")
-parser.add_argument("--stop", type=float, default=3, help=" - The end of the coordinate plane")
-parser.add_argument("--resolution", "--res", type=float, default=0.05, help=" - The resolution of the SAR image")
+parser.add_argument("--filename", "-f", type=str, required=True, help=" - The SAR data filename.")
+parser.add_argument("--start", type=float, default=-3, help=" - The start of the coordinate plane.")
+parser.add_argument("--stop", type=float, default=3, help=" - The end of the coordinate plane.")
+parser.add_argument("--resolution", "--res", type=float, default=0.05, help=" - The resolution of the SAR image.")
 parser.add_argument("--two_dimensional_range_bins", "--2D-range-bins",  "--2D_bins", action="store_true", help=" - Enables use of weird 2D range_bins.")
 parser.add_argument("--mode", "-m", type=str, default="2dfast", help=" - The mode to run back projection in.")
-parser.add_argument("--realign", "--align", action="store_true", help=" - Realigns the motion capture data and the radar data")
+parser.add_argument("--realign", "--align", action="store_true", help=" - Realigns the motion capture data and the radar data.")
+parser.add_argument("--crop", "-c", nargs=2, metavar=("start", "stop"), help=" - Crops the corner reflectors out using start and stop positions.")
+parser.add_argument("--normalize", "--norm", action="store_true", help=" - Uses the range_norm function to account for different ranges in the list of intesities")
+parser.add_argument("--clim", nargs=2, help=" - Color map stuff")
+parser.add_argument("--reflectors", type=int, default=1, help=" - Number of corner reflectors")
 args = parser.parse_args()
+
+print(args)
 
 with open(args.filename, 'rb') as f:
     data = pickle.load(f)
 
-print(args)
-print(data)
+data = replace_nans(data)
 
+if args.two_dimensional_range_bins:
+    data['range_bins'] = data['range_bins'][0]
+    
 if args.realign:
-    bpdat = backprojection(motion_align(replace_nans(data)), args.start,args.stop, args.resolution, args.two_dimensional_range_bins, args.mode)
-else:
-    bpdat = backprojection(replace_nans(data), args.start,args.stop, args.resolution, args.two_dimensional_range_bins, args.mode)
+    data = motion_align(data)
+
+if args.normalize:
+    data = range_norm(data)
+
+plt.imshow(np.abs(data['scan_data']),
+    extent=(
+        data['range_bins'][0],
+        data['range_bins'][-1],
+        data['scan_timestamps'][-1] - data['scan_timestamps'][0],
+        0))
+
+plt.xlabel('Range (m)')
+plt.ylabel('Elapsed Time (s)')
+
+for refnum in range(args.reflectors):
+    r1 = np.sqrt(np.sum(
+            (data['platform_pos'] - data['corner_reflector_pos'][refnum, :])**2, 1))
+    plt.plot(r1, data['motion_timestamps'], 'r--', label='Corner Reflector' + str(refnum + 1))
+plt.show()
+
+if args.crop is not None:
+    data = crop(data, float(args.crop[0]), float(args.crop[1]))
+
+print(data)
+bpdat = backprojection(data, args.start, args.stop, args.resolution, args.mode)
 
 plt.xlabel('X (m)')
 plt.ylabel('Z (m)')
@@ -214,25 +289,6 @@ plt.imshow(bpdat,
         args.stop,
         args.stop,
         args.start))
-plt.clim(100, 150000)
+##plt.clim(100)
 plt.colorbar()
-plt.show()
-
-data = motion_align(data)
-
-plt.imshow(np.abs(data['scan_data']),
-    extent=(
-        data['range_bins'][0, 0],
-        data['range_bins'][0, -1],
-        data['scan_timestamps'][-1] - data['scan_timestamps'][0],
-        0))
-
-plt.xlabel('Range (m)')
-plt.ylabel('Elapsed Time (s)')
-r1 = np.sqrt(np.sum(
-        (data['platform_pos'] - data['corner_reflector_pos'][0, :])**2, 1))
-r2 = np.sqrt(np.sum(
-        (data['platform_pos'] - data['corner_reflector_pos'][1, :])**2, 1))
-plt.plot(r1, data['motion_timestamps'], 'r--', label='Corner Reflector 1')
-plt.plot(r2, data['motion_timestamps'], 'r--', label='Corner Reflector 2')
 plt.show()
